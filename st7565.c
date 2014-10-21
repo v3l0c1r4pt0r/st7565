5b623e477751ff8a9be27b5bcd16203819943d63
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2014-10-21 19:51:08 +0200

    Added sysrq just at the beginning of init_module(); license and auth info

--- st7565.c
+++ st7565.c
@@ -1,9 +1,11 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/sysrq.h>
 
 int init_module()
 {
   printk(KERN_INFO "module loaded\n");
+  handle_sysrq('g');	// st7565.ko+0x24
   return 0;
 }
 
@@ -11,3 +13,6 @@ void cleanup_module()
 {
   printk(KERN_INFO "module unloaded\n");
 }
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Kamil Lorenc <v3l0c1r4pt0r_at_gmail_dot_com>");
