213e60024c29a35fd4278ee7763eb585cb9fc57b
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2014-11-21 17:14:40 +0100

    Implemented llseek

--- st7565.c
+++ st7565.c
@@ -17,12 +17,40 @@ static int __init st7565_init(void)
     memset(st.buffer, 0, LCD_BUFF_SIZE);
     //comment out if not debug
     /*FIXME:TMP*/
-    unsigned char buf[] = "\0\1\2\3\4\5\6\7\10\11\12\13\14\15\16\17\20\21\22\23\24\25\26\27\30\31\32\33\34\35\36\37"
+    unsigned char buf[] = //"\0\1\2\3\4\5\6\7\10\11\12\13\14\15\16\17\20\21\22\23\24\25\26\27\30\31\32\33\34\35\36\37"
                           " !\"#$%&'()*+,-./0123456789:;<=>?"
                           "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
-                          "`abcdefghijklmnopqrstuvwxyz{|}~\177";
-    memcpy(st.buffer, buf, 128);
-    st.buffer[128]='\0';
+                          "`abcdefghijklmnopqrstuvwxyz{|}~\177"
+			  "e4htewfne8n4guiengeuihfre78bgf87"
+			  "p[dlf][ewo04owe[_{O{)i9pJo9jO[0I"
+			  "78hiUG&*G&*g*&g&g*&FG7F6%d%$6d4S"
+			  "f%^YTf%d%$ds235s5d6f7&^f^7fG&*(h"
+			  ":Llp[p[;p[lplooikKIOPLp[l[lppkok"
+                          " !\"#$%&'()*+,-./0123456789:;<=>?"
+                          "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
+                          "`abcdefghijklmnopqrstuvwxyz{|}~\177"
+			  "e4htewfne8n4guiengeuihfre78bgf87"
+			  "p[dlf][ewo04owe[_{O{)i9pJo9jO[0I"
+			  "78hiUG&*G&*g*&g&g*&FG7F6%d%$6d4S"
+			  "f%^YTf%d%$ds235s5d6f7&^f^7fG&*(h"
+			  ":Llp[p[;p[lplooikKIOPLp[l[lppkok"
+                          " !\"#$%&'()*+,-./0123456789:;<=>?"
+                          "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
+                          "`abcdefghijklmnopqrstuvwxyz{|}~\177"
+			  "e4htewfne8n4guiengeuihfre78bgf87"
+			  "p[dlf][ewo04owe[_{O{)i9pJo9jO[0I"
+			  "78hiUG&*G&*g*&g&g*&FG7F6%d%$6d4S"
+			  "f%^YTf%d%$ds235s5d6f7&^f^7fG&*(h"
+			  ":Llp[p[;p[lplooikKIOPLp[l[lppkok"
+                          " !\"#$%&'()*+,-./0123456789:;<=>?"
+                          "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
+                          "`abcdefghijklmnopqrstuvwxyz{|}~\177"
+			  "e4htewfne8n4guiengeuihfre78bgf87"
+			  "p[dlf][ewo04owe[_{O{)i9pJo9jO[0I"
+			  "78hiUG&*G&*g*&g&g*&FG7F6%d%$6d4S"
+			  "f%^YTf%d%$ds235s5d6f7&^f^7fG&*(h"
+			  ":Llp[p[;p[lplooikKIOPLp[l[lppkok";
+    memcpy(st.buffer, buf, LCD_BUFF_SIZE);
     /*FIXME:END*/
     static struct file_operations fops =
     {
@@ -132,10 +160,10 @@ static ssize_t glcd_read(struct file *filp,	/* see include/linux/fs.h   */
     /*
      * Number of bytes actually written to the buffer
      */
-printk(KERN_INFO "length: 0x%X; buffer: 0x%X; fpos: 0x%X\n", length, st.buffer, filp->f_pos);
+// printk(KERN_INFO "length: 0x%X; buffer: 0x%X; fpos: 0x%X\n", length, st.buffer, filp->f_pos);
     if(length + filp->f_pos > LCD_BUFF_SIZE)
         length = LCD_BUFF_SIZE - filp->f_pos;
-printk(KERN_INFO "length: 0x%X; buffer: 0x%X; fpos: 0x%X\n", length, st.buffer, filp->f_pos);
+// printk(KERN_INFO "length: 0x%X; buffer: 0x%X; fpos: 0x%X\n", length, st.buffer, filp->f_pos);
     /*
      * If we're at the end of the message,
      * return 0 signifying end of file
@@ -172,6 +200,21 @@ static ssize_t glcd_write(struct file *filp, const char *buff, size_t len, loff_
 
 static loff_t glcd_llseek(struct file * filp, loff_t off, int whence)
 {
-    //TODO: change position according to off and whence; update position on ST7565
+    //TODO: update position on ST7565
+    switch(whence)
+    {
+      case SEEK_SET:
+	filp->f_pos = off;
+	break;
+      case SEEK_CUR:
+	filp->f_pos += off;
+	break;
+      case SEEK_END:
+	filp->f_pos = LCD_BUFF_SIZE + off;
+	break;
+      default:
+	printk(KERN_INFO "I wasn't expected to get here!\n");
+	//FIXME: what about the case when f_pos is beeing set outside of buffer, what would be read then?
+    }
     return filp->f_pos;
 }
